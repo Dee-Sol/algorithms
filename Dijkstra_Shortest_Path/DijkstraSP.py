4b598b342b614ba4d480aba7ae32a5bbe0097de8
DS <93722733+Dee-Sol@users.noreply.github.com>
2022-07-25 17:32:50 +0200

    Update DijkstraSP.py
    Amend variables order in heaps to facilitate Extract-Min

--- Dijkstra_Shortest_Path/DijkstraSP.py
+++ Dijkstra_Shortest_Path/DijkstraSP.py
@@ -11,7 +11,6 @@ import math # for inf method
 import heapq # for heaps data structures
 import utils # helper module for graph import
 
-
 #%% Define Algorithm
 
 def Dijkstra_SP(graph: dict, start = 1) -> dict:
@@ -33,14 +32,15 @@ def Dijkstra_SP(graph: dict, start = 1) -> dict:
     for vertex in graph:
         if vertex != start:
             distances[vertex] = math.inf
-    to_check = [(start, 0)]
+    # Note variables order in the tuple - distance is first, to facilite heap's Extract-Min
+    to_check = [(0, start)]
     while to_check:
-        current_vertex, total = heapq.heappop(to_check)
+        total, current_vertex = heapq.heappop(to_check)
         for neighbor, distance in graph[current_vertex]:
             new_total = total + distance
             if new_total < distances[neighbor]:
                 distances[neighbor] = new_total
-                heapq.heappush(to_check, (neighbor, new_total))
+                heapq.heappush(to_check, (new_total, neighbor))
     return distances
 
 #%% Define Main
@@ -48,11 +48,12 @@ def Dijkstra_SP(graph: dict, start = 1) -> dict:
 def main(filename: str, destination: list, start_vertex = 1):
     G = utils.import_graph(filename)
     dist = Dijkstra_SP(G, start_vertex)
-    
+
     print(f'~~~~~~~~~~~~~~~~~~~~~~~ \nShortest distance from vertex {start_vertex}:')
     
     for vertex in destination:
         print(f'To vertex {vertex}: {dist[vertex]}')
+
 
 #%% Run Main
 
